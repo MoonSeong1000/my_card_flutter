fe3f2f8dc5e7328e25651b52d547170f24c68acb
cms9586 <cms9586@naver.com>
2020-09-22 23:37:31 +0900

    chage card Widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
@@ -38,53 +39,46 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.bold
                 )
               ),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child:Row(
-                  children: [
-                    Icon(
-                        Icons.phone,
-                        color:Colors.teal,
+              SizedBox(height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal.shade100,
+              )),
+              Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color:Colors.teal,
                     ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      "+82 010 1234 5678",
+                    title: Text(
+                      "82+ 010 1234 5678",
                       style: TextStyle(
                         color: Colors.teal.shade900,
                         fontFamily: 'Sans',
                         fontSize: 20.0,
                       ),
                     ),
-                  ],
-                )
-              ),
-              Container(
-                  padding: EdgeInsets.all(10.0),
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  child:Row(
-                    children: [
-                      Icon(
-                        Icons.email,
-                        color:Colors.teal,
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                        "cms9586@naver.com",
-                        style: TextStyle(
-                          color: Colors.teal.shade900,
-                          fontFamily: 'Sans',
-                          fontSize: 20.0,
-                        ),
-                      ),
-                    ],
                   )
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color:Colors.teal,
+                  ),
+                  title: Text(
+                    "cms9586@naver.com",
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Sans',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                )
               )
             ]
           )
